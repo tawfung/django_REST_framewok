c3c7f0c36b49feac8b10d058cd09c5aebb12f5a0
tawfung <tungvanle@gmail.com>
2017-12-27 17:08:00 +0700

    Core API - Schemas and Client

--- django_REST/tutorial/snippets/views.py
+++ django_REST/tutorial/snippets/views.py
@@ -168,7 +168,6 @@ class SnippetViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
-
     Additionally we also provide an extra `highlight` action.
     """
     queryset = Snippet.objects.all()
